c13bde41add64953c833dcc89171a3ffe2a0aa4f
jonschlinkert <jon.schlinkert@sellside.com>
2018-08-14 12:50:47 -0400

    refactor

--- index.js
+++ index.js
@@ -1,33 +1,159 @@
 'use strict';
 
-var extend = require('extend-shallow');
-var formatters = require('./lib/formatters');
-
 /**
  * Format a list of people like objects (e.g. authors, contributors, and collaborators)
  * into the specified format.
  *
  * ```js
- * var people = [
- *   { name: 'Brian Woodward' }
- * ];
- * var table = format(people, {format: 'table'});
+ * const people = [ { name: 'Brian Woodward' } ];
+ * const table = format(people, 'table');
  * console.log(table);
  * ```
- * @param  {Array} `arr` Array of people objects.
- * @param  {Object} `options` Additional options
- * @param  {String} `options.format` Formatter function used to format the array. See [formatters](#formatters) for more details.
- * @return {Mixed} Formatted array of people. Returned type depends on formatter.
+ * @param {Array} `arr` Array of people objects.
+ * @param {String} `format` The format "type" to use. Valid types are `table`, `list` and `aligned`. If no type is passed, `table` will be used automatically.
+ * @return {String} Formatted
  * @api public
  */
 
-module.exports = function format(arr, options) {
-  var opts = extend({format: 'noop'}, options);
-  return formatters[opts.format](arr);
+const format = (arr, name) => {
+  return format[name] ? format[name](arr) : format.table(arr);
 };
 
 /**
- * Expose formatters object.
+ * Returns the array of people formatted as a markdown table.
+ *
+ * ```js
+ * const people = [
+ *   { login: 'doowb', contributions: 100, html_url: 'https://github.com/doowb' },
+ *   { login: 'jonschlinkert', contributions: 50, html_url: 'https://github.com/jonschlinkert' }
+ * ];
+ * console.log(format.table(people));
+ * //=> | **Commits** | **Contributor**<br/> |
+ * //=> | --- | --- |
+ * //=> | 100 | [doowb](https://github.com/doowb) |
+ * //=> | 50 | [jonschlinkert](https://github.com/jonschlinkert) |
+ * ```
+ * @param {Array} `arr` Array of people to format.
+ * @return {String} Markdown table
+ * @api public
  */
 
-module.exports.formatters = formatters;
+format.table = list => {
+  let arr = [].concat(list || []);
+
+  if (arr.length === 0) {
+    return '';
+  }
+
+  let max = longest(arr, 'contributions');
+  let res = '| **Commits** | **Contributor** |  ';
+  res += '\n';
+  res += '| --- | --- |  ';
+  res += '\n';
+
+  arr.forEach(person => {
+    res += '| ';
+    res += String(person.contributions || '').padEnd(max, ' ');
+    res += ' | ';
+    res += link(person.login, person.html_url);
+    res += ' |  ';
+    res += '\n';
+  });
+
+  return res;
+};
+
+/**
+ * Returns the array of people formatted as a markdown list.
+ *
+ * ```js
+ * const people = [
+ *   { login: 'doowb', contributions: 100, html_url: 'https://github.com/doowb' },
+ *   { login: 'jonschlinkert', contributions: 50, html_url: 'https://github.com/jonschlinkert' }
+ * ];
+ * console.log(format.list(people));
+ * //=> **Commits** / **Contributor**
+ * //=> + 100 [doowb](https://github.com/doowb)
+ * //=> + 50 [jonschlinkert](https://github.com/jonschlinkert)
+ * ```
+ * @param {Array} `arr` Array of people to format.
+ * @return {String} Markdown list
+ * @api public
+ */
+
+format.list = list => {
+  let arr = [].concat(list || []);
+
+  if (arr.length === 0) {
+    return '';
+  }
+
+  let max = longest(arr, 'contributions');
+  let res = '**Commits** / **Contributor**\n';
+
+  arr.forEach(person => {
+    res += '+ ';
+    res += String(person.contributions || '').padEnd(max, ' ');
+    res += ' ';
+    res += link(person.login, person.html_url);
+    res += '\n';
+  });
+  return res;
+};
+
+/**
+ * Returns the array of people formatted as a GitHub Flavored Markdown fenced code block,
+ * with aligned columns.
+ *
+ * ```js
+ * const people = [
+ *   { login: 'doowb', contributions: 100, html_url: 'https://github.com/doowb' },
+ *   { login: 'jonschlinkert', contributions: 50, html_url: 'https://github.com/jonschlinkert' }
+ * ];
+ * console.log(format.aligned(people));
+ * //=> COMMITS / CONTRIBUTOR
+ * //=> ------- | -----------
+ * //=> 100      doowb
+ * //=> 50      jonschlinkert
+ * ```
+ * @param {Array} `arr` Array of people to format.
+ * @return {String} Markdown code block with aligned columns.
+ * @api public
+ */
+
+format.aligned = list => {
+  let arr = [].concat(list || []);
+
+  if (arr.length === 0) {
+    return '';
+  }
+
+  let max = longest(arr, 'contributions');
+  let res = '```bash\n';
+  res += 'COMMITS / CONTRIBUTOR\n';
+  res += '------- | -----------\n';
+
+  arr.forEach(person => {
+    res += String(person.contributions || '').padEnd(max, ' ');
+    res += '      ';
+    res += person.login;
+    res += '\n';
+  });
+
+  res += '```\n';
+  return res;
+};
+
+function longest(arr, prop) {
+  return arr.reduce((n, c) => Math.max((c[prop] + '').length, n), 0);
+}
+
+function link(anchor, href, title) {
+  return anchor && href ? `[${anchor}](${href}${title ? ` "${title}"` : ''})` : '';
+}
+
+/**
+ * Expose format
+ */
+
+module.exports = format;
--- test.js
+++ test.js
@@ -1,16 +1,48 @@
 'use strict';
 
 require('mocha');
-var assert = require('assert');
-var format = require('./');
+const assert = require('assert');
+const format = require('./');
 
-describe('format-people', function() {
-  it('should export a function', function() {
+const fixtures = [
+  { login: 'jonschlinkert', html_url: 'https://github.com/jonschlinkert', contributions: 11 },
+  { login: 'doowb', html_url: 'https://github.com/doowb', contributions: 1111 }
+];
+
+describe('format-people', () => {
+  it('should export a function', () => {
     assert.equal(typeof format, 'function');
   });
 
-  it('should export a formatters object', function() {
-    assert(format.formatters);
-    assert.equal(typeof format.formatters, 'object');
+  it('should expose formatter methods', () => {
+    assert.equal(typeof format.table, 'function');
+    assert.equal(typeof format.aligned, 'function');
+    assert.equal(typeof format.list, 'function');
+  });
+
+  it('should format a table', () => {
+    assert.equal(format.table(fixtures), '| **Commits** | **Contributor** |  \n| --- | --- |  \n| 11   | [jonschlinkert](https://github.com/jonschlinkert) |  \n| 1111 | [doowb](https://github.com/doowb) |  \n');
+  });
+
+  it('should format a list', () => {
+    assert.equal(format.list(fixtures), '**Commits** / **Contributor**\n+ 11   [jonschlinkert](https://github.com/jonschlinkert)\n+ 1111 [doowb](https://github.com/doowb)\n');
+  });
+
+  it('should format an aligned list', () => {
+    assert.equal(format.aligned(fixtures), '```bash\nCOMMITS / CONTRIBUTOR\n------- | -----------\n11        jonschlinkert\n1111      doowb\n```\n');
   });
 });
+
+//     ✓ should format a table
+// **Commits** / **Contributor**
+// + 11   [jonschlinkert](https://github.com/jonschlinkert)
+// + 1111 [doowb](https://github.com/doowb)
+
+//     ✓ should format a list
+// ```bash
+// COMMITS / CONTRIBUTOR
+// ------- | -----------
+// 11        jonschlinkert
+// 1111      doowb
+// ```
+// `
